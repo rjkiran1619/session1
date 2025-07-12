3728c681ac5587d8cd852b62dc9136eb30fa3fca
Raj Kiran <rjkiran1619@gmail.com>
2025-07-12 12:48:20 +0530

    this is the second program

---
+++ src/water/loop.java
@@ -0,0 +1,11 @@
+package water;
+
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a = 10;
+   int b;
+System.out.println("this is  add");
+}
+}
